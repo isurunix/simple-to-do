12d6c27ce8143f464a653a220fb13d1d7d75dc10
isurunix <isurunix@gmail.com>
2017-06-20 21:25:25 +0530

    Minor refactoring

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var app = angular.module('app',[]);
 
 app.controller('todo-controller', ['$scope', function($scope) {
-  $scope.items = ['item1','item2'];
-  $scope.add = function(todo){
-    $scope.items.push(todo.item);
-    console.log($scope.items);
-    todo.item='';
+
+  $scope.appContext = { items: ['item1','item2'] };
+
+  $scope.methods = {
+
+    add: function(todo) {
+      $scope.appContext.items.push(todo.item);
+      console.log($scope.appContext.items);
+      todo.item = '';
+    },
+
+    remove: function(item) {
+        console.log('item:' + item);
+        var index = $scope.appContext.items.indexOf(item);
+        console.log('index:' + index);
+        var removed = $scope.appContext.items.splice(index, 1);
+        console.log('removed:' + removed);
+        console.log($scope.appContext.items);
+    }
   };
-  $scope.remove = function(item){
-    console.log(item);
-    var index = $scope.items.indexOf(item);
-    console.log(index);
-    var removed = $scope.items.splice(index, 1);
-    console.log(removed);
-    console.log($scope.items);
-  };
+
 }]);
 
 app.component('toDoItem',{
   templateUrl: 'components/todo.html',
-  bindings: {text:'@'},
+  bindings: {
+    text:'@',
+    methods: '@'
+  },
 });
