d6b1f97620b96862d50ed0d6a92ab267bec32962
isurunix <isurunix@gmail.com>
2017-07-23 00:19:29 +0530

    Added gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var webserver = require('gulp-webserver');
+
+
+
+gulp.task('serve', function () {
+	gulp.src('app')
+		.pipe(webserver({
+			livereload: true,
+			directoryListing: false,
+			port: 9000
+		}))
+})
